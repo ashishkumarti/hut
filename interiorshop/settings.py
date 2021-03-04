ba444a25b25952ddfa74c186573afeb5220bb4bc
ashishkumarti <ashish23by6@gmail.com>
2021-03-05 01:39:16 +0530

    loadstatic

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -144,8 +144,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_ROOT = os.path.join (BASE_DIR,'media')
-MEDIA_URL ='/ media /'
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"static","media")
+MEDIA_URL ='/media/'
