298260a9bd8a64c32fdedf80ad4468e53b9f2bd8
ashishkumarti <ashish23by6@gmail.com>
2021-03-05 03:00:47 +0530

    url

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-#from django.views.static import serve
-#from django.conf.urls.static import url
+from django.views.static import serve
+from django.conf.urls.static import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +27,7 @@ urlpatterns = [
     path('cart/', include('apps.cart.urls')),
     path('', include('apps.core.urls')),
     path('', include('apps.product.urls')),
-#    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-#    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] 
 urlpatterns=urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
