37c2923101c0d1e7e452745bb9c461eca85d644e
ashishkumarti <ashish23by6@gmail.com>
2021-03-05 00:24:49 +0530

    requirements_edit

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+#from django.views.static import serve
+#from django.conf.urls.static import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vendors/', include('apps.vendor.urls')),
@@ -25,5 +28,5 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('', include('apps.product.urls')),
     #url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
