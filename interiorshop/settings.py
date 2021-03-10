e0fffa89d762fb8098d304c85af4ff572cad23ca
ashishkumarti <ashish23by6@gmail.com>
2021-03-10 21:45:35 +0530

    requirements

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -174,3 +174,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
