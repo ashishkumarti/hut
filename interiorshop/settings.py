b6c9f5801f9394984546b03ba910a83dfb8c083c
ashishkumarti <ashish23by6@gmail.com>
2021-03-10 20:34:28 +0530

    postgresql

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -100,8 +100,15 @@ WSGI_APPLICATION = 'interiorshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hut',
+        'USER': 'postgres',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
